89ed8d3a1d8e340830736e3724cd92bbc761e7bd
Andrey Filimonov <gipget@mail.ru>
2017-07-14 19:27:48 +0300

    corrected

--- js.js
+++ js.js
@@ -1,16 +1,21 @@
 let inp1 = document.querySelector('.inp1');
 let inp2 = document.querySelector('.inp2');
 let button = document.querySelector('button');
+let users = {
+    ivan: '333',
+    ssss: '665',
+    gibs: '0000'
+}
+
 
 button.addEventListener('click', checkLogin);
 
 function checkLogin(){    
-    if(inp1.value == 'ivan' && inp2.value == '333' || inp1.value == 'ssss' && inp2.value == '665' || inp1.value == 'gibs' && inp2.value == '0000' ){
-       alert('Добро пожаловать');
-    }
-    else{        
-        alert('ошибка');        
-    }
+    users[inp1.value] === inp2.value ? alert('Добро пожаловать') : alert('ошибка');
     inp1.value = '';
     inp2.value = '';
 }
+
+
+
+
